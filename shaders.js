e0f548c3c14318dc7d8887482ee216e03c3d62e4
platote <84842352+platote@users.noreply.github.com>
2022-03-21 12:01:13 +0100

    Add files via upload

--- shaders.js
+++ shaders.js
@@ -2,10 +2,17 @@ import * as THREE from 'three';
 
 const vertexShaderSource = `
 	${THREE.ShaderChunk.common}
+
+  varying vec2 vUv;
+
 	${THREE.ShaderChunk.logdepthbuf_pars_vertex}
+  uniform float u_time;
 
 void main() {
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+  vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+  modelPosition.y += abs(pow(modelPosition.x, 3.0))/2.0  * sin(u_time * 3.0)  / 8.0 ;
+  gl_Position = projectionMatrix * viewMatrix * modelPosition;
+  vUv = uv;
   ${THREE.ShaderChunk.logdepthbuf_vertex}
 }`;
 
@@ -14,11 +21,15 @@ const fragmentShaderSource = `
   precision mediump float;
   ${THREE.ShaderChunk.logdepthbuf_pars_fragment}
 
-  uniform float u_time;
+  // uniform sampler2D uTexture;
+
+  varying vec2 vUv;
 
   void main() {
 
-  gl_FragColor = vec4(abs(sin(u_time)), 0.0, 0.0, 1.0);
+  // vec4 textureColor = texture2D(uTexture, vUv);
+  // gl_FragColor = textureColor;
+  gl_FragColor = vec4(0.11, 0.88, 1.0, 0.5);
   ${THREE.ShaderChunk.logdepthbuf_fragment}
 }`;
 
